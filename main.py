06d5a9a135835f33d6d64d6442a9dde88286d96a
Miriel <me@miriel.net>
2024-06-29 18:08:18 +0900

    Fix: 追加忘れ

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from app.endpoints.api import index as APIIndex
 from app.endpoints.api.users import stat as UserStat
 from app.endpoints.api.auth import register, login
 from app.endpoints.api.timeline import local
+from app.endpoints.api.letter.create import router as create_letter_router  # 追加
 from app.endpoints.wellknown.nodeinfo import router as nodeinfo_router  # 追加
 
 log = logging.getLogger("uvicorn")
@@ -27,6 +28,7 @@ app.include_router(UserStat.router)
 app.include_router(register.router)
 app.include_router(login.router)
 app.include_router(local.router)
+app.include_router(create_letter_router)  # 追加
 app.include_router(nodeinfo_router)  # 追加
 
 # Static files setup
