f5d4f4390b1aeeeaf5e7247f4aa16347af2a5e0c
ねんねこ <nennneko5787@gmail.com>
2024-07-06 08:51:22 +0900

    frontend.py を更新

---
+++ app/endpoints/frontend.py
@@ -0,0 +1,25 @@
+import aiofiles
+from fastapi import APIRouter
+
+router = APIRouter()
+
+# HTML response endpoints
+@router.api_route("/", methods=['GET', 'HEAD'], response_class=HTMLResponse, include_in_schema=False)
+async def root():
+    async with aiofiles.open("pages/index.html", "r", encoding="utf8") as f:
+        return HTMLResponse(await f.read())
+
+@router.api_route("/timeline", methods=['GET', 'HEAD'], response_class=HTMLResponse, include_in_schema=False)
+async def timeline():
+    async with aiofiles.open("pages/timeline.html", "r", encoding="utf8") as f:
+        return HTMLResponse(await f.read())
+
+@router.api_route("/login", methods=['GET', 'HEAD'], response_class=HTMLResponse, include_in_schema=False)
+async def login():
+    async with aiofiles.open("pages/login.html", "r", encoding="utf8") as f:
+        return HTMLResponse(await f.read())
+
+@router.api_route("/register", methods=['GET', 'HEAD'], response_class=HTMLResponse, include_in_schema=False)
+async def register():
+    async with aiofiles.open("pages/register.html", "r", encoding="utf8") as f:
+        return HTMLResponse(await f.read())
