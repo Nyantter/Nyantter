c414843495efbf322160ce8a7ec22a1dff241c95
ねんねこ <nennneko5787@gmail.com>
2024-07-06 07:48:40 +0900

    もういやです

--- app/endpoints/api/timeline/local.py
+++ app/endpoints/api/timeline/local.py
@@ -13,53 +13,18 @@ router = APIRouter()
 def isEmoji(char: str):
     return char in emoji.EMOJI_DATA
 
-async def get_current_user(authorization: Optional[str] = Header(...)):
-    if authorization is None:
-        return {"id": 0}
-
-    try:
-        token = authorization.split(" ")[1]  # "Bearer <token>"
-    except IndexError:
-        return {"id": 0}
-
-    conn: asyncpg.Connection = await asyncpg.connect(
-        host=DataHandler.database["host"],
-        port=DataHandler.database["port"],
-        user=DataHandler.database["user"],
-        password=DataHandler.database["pass"],
-        database=DataHandler.database["name"]
-    )
-
-    user_id = await conn.fetchval(f"SELECT user_id FROM {DataHandler.database['prefix']}tokens WHERE token = $1", token)
-
-    if not user_id:
-        await conn.close()
-        return {"id": 0}
-
-    user = await conn.fetchrow(f"SELECT * FROM {DataHandler.database['prefix']}users WHERE id = $1", user_id)
-
-    await conn.close()
-
-    if not user:
-        return {"id": 0}
-
-    return dict(user)
-
 @router.get(
     "/api/timeline/local",
     response_class=JSONResponse,
     summary="ローカルタイムラインを取得します。"
 )
 async def localTimeLine(
-    user: Optional[dict] = Depends(get_current_user),
     page: int = Query(default=0, ge=0),
     since: Optional[str] = None
 ):
     """
     ローカルタイムラインを取得します。
     """
-    
-    user_id = user.get("id", 0)
     
     if since is None:
         since = datetime(2000, 1, 1)
@@ -93,10 +58,6 @@ async def localTimeLine(
         reactions = []
         for _emoji in emojis:
             _emoji = dict(_emoji)
-            if _emoji.get("user_id") == user_id:
-                _emoji["ismine"] = True
-            else:
-                _emoji["ismine"] = False
             pattern = r'^:([A-Za-z0-9_]+):$'
             match = re.match(pattern, _emoji["reaction"])
 
