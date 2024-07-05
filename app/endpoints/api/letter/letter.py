3cbe2e390dadb91d9fa52c2a77493287bcd4632c
ねんねこ <nennneko5787@gmail.com>
2024-07-06 07:49:31 +0900

    letter.py を更新

--- app/endpoints/api/letter/letter.py
+++ app/endpoints/api/letter/letter.py
@@ -16,49 +16,15 @@ router = APIRouter()
 def isEmoji(char: str):
     return char in emoji.EMOJI_DATA
 
-async def get_current_user(authorization: Optional[str] = Header(...)):
-    if authorization is None:
-        return {"id": 0}
-
-    try:
-        token = authorization.split(" ")[1]  # "Bearer <token>"
-    except IndexError:
-        return {"id": 0}
-
-    conn: asyncpg.Connection = await asyncpg.connect(
-        host=DataHandler.database["host"],
-        port=DataHandler.database["port"],
-        user=DataHandler.database["user"],
-        password=DataHandler.database["pass"],
-        database=DataHandler.database["name"]
-    )
-
-    user_id = await conn.fetchval(f"SELECT user_id FROM {DataHandler.database['prefix']}tokens WHERE token = $1", token)
-
-    if not user_id:
-        await conn.close()
-        return {"id": 0}
-
-    user = await conn.fetchrow(f"SELECT * FROM {DataHandler.database['prefix']}users WHERE id = $1", user_id)
-
-    await conn.close()
-
-    if not user:
-        return {"id": 0}
-
-    return dict(user)
-
 @router.get(
     "/api/letter/{letter_id:int}",
     response_class=JSONResponse,
     summary="レターの情報を取得します。"
 )
-async def letter(letter_id: int, user: Optional[dict] = Depends(get_current_user)):
+async def letter(letter_id: int):
     """
     レターの情報を取得します。
     """
-    
-    user_id = user.get("id", 0)
     
     conn: asyncpg.Connection = await asyncpg.connect(
         host=DataHandler.database["host"],
@@ -77,11 +43,6 @@ async def letter(letter_id: int, user: Optional[dict] = Depends(get_current_user
     reactions = []
     for _emoji in emojis:
         _emoji = dict(_emoji)
-        
-        if _emoji.get("user_id") == user_id:
-            _emoji["ismine"] = True
-        else:
-            _emoji["ismine"] = False
         
         pattern = r'^:([A-Za-z0-9_]+):$'
         match = re.match(pattern, _emoji["reaction"])
