481dfff163b88aef5e3c5778d988da3e036a3f64
ねんねこ <nennneko5787@gmail.com>
2024-07-06 14:43:35 +0900

    Fix: Snowflakeは絶対にかぶらせません

--- app/snowflake.py
+++ app/snowflake.py
@@ -2,10 +2,11 @@ from snowflake import SnowflakeGenerator
 from datetime import datetime
 
 class Snowflake:
+    gen = SnowflakeGenerator(
+        instance=406,
+        epoch=0,
+        timestamp=int(datetime.now().timestamp() * 1000),
+    )
+    
     def generate() -> int:
-        gen = SnowflakeGenerator(
-            instance=406,
-            epoch=0,
-            timestamp=int(datetime.now().timestamp() * 1000),
-        )
         return next(gen)
